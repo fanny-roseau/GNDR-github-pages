05bd592597ef261a3443126e1c63e6fd162a8e39
felixperroud <felix.perroud@gmail.com>
2017-12-18 16:37:48 +0100

    MANYTHINGS

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 
 $(document).ready(function(){
-$(".disorder_container").click(function() {
-	$(".disorder_container, .landing_img").hide();
-	$(".content").removeClass("img_content");
-	$(".content").addClass("video_content"); 
-	$(".img_container").css({"width": "none", "height": "100%"});
-
-
-	$(".landing_video").show();
-	$(".landing_video").get(0).play();
- });
-
-$(".landing_video").on("ended", function() {
-	$(".disorder_container, .landing_img").show();
-	// $(".img_container").css("width", "calc(100vh - 310px)");
-	$(".landing_video").hide();
-	$(".content").addClass("img_content");
-	$(".content").removeClass("video_content");
-
-})
+// $(".disorder_container").click(function() {
+// 	$(".disorder_container, .landing_img").hide();
+// 	$(".content").removeClass("img_content");
+// 	$(".content").addClass("video_content");
+// 	$(".img_container").css({"width": "none", "height": "100%"});
+//
+// 
+// 	$(".landing_video").show();
+// 	$(".landing_video").get(0).play();
+//  });
+//
+// $(".landing_video").on("ended", function() {
+// 	$(".disorder_container, .landing_img").show();
+// 	// $(".img_container").css("width", "calc(100vh - 310px)");
+// 	$(".landing_video").hide();
+// 	$(".content").addClass("img_content");
+// 	$(".content").removeClass("video_content");
+//
+// })
 
 $('.dropdown').hover(function(e) {
      $('.space').css("display", "block"); },
